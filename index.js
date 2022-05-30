2288abfc6564051d0cf36794e95b4ca98dc44dcb
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-05-30 09:31:37 -0400

    updated index.js and tested in console. need to iron out file creation.

--- index.js
+++ index.js
@@ -57,56 +57,115 @@ const startApp = () => {
 
   function createTeam() {
     inquirer
-    .prompt({
-        type: 'list',
-        name: 'memberType',
-        message: 'Which type of team member would you like to add?',
-        choices: [
-          'Engineer',
-          'Intern',
-          "None. I have added all team members.",
-        ],
-    })
-    .then((userChoice) =>{
-      switch (userChoice.memberType) {
-        case 'Engineer':
-          addEngineer();
-          break;
-        case 'Intern':
-          addIntern();
-          break;
-        default:
-          buildTeam();
-      }
-    });
+      .prompt([
+        {
+          type: "list",
+          name: "memberType",
+          message: "Which type of team member would you like to add?",
+          choices: [
+            "Engineer",
+            "Intern",
+            "None. I have added all team members.",
+          ],
+        },
+      ])
+      .then((userChoice) => {
+        switch (userChoice.memberType) {
+          case "Engineer":
+            addEngineer();
+            break;
+          case "Intern":
+            addIntern();
+            break;
+          default:
+            buildTeam();
+        }
+      });
   }
 
-  function addEngineer()
+  function addEngineer() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "engineerName",
+          message: "What is the name of the engineer?",
+        },
+        {
+          type: "input",
+          name: "engineerId",
+          message: "What is the id of the engineer?",
+        },
 
-  function addIntern()
-  // inquirer
-  //   .prompt([
-  //     {
-  //       type: "input",
-  //       name: "name",
-  //       message: "What's your name?",
-  //     },
-  //     {
-  //       type: "input",
-  //       name: "email",
-  //       message: "What's your email?",
-  //     },
-  //     {
-  //       type: "input",
-  //       name: "id",
-  //       message: "What's your ID?",
-  //     },
-  //   ])
-  //   .then((res) => {
-  //     console.log(res);
-  //     const newEmployee = new Employee(res.name, res.id, res.email);
-  //     fs.writeFileSync("./dist/employee.html", makePage(newEmployee), "utf-8");
-  //   });
+        {
+          type: "input",
+          name: "engineerEmail",
+          message: "What is the email of the engineer?",
+        },
+        {
+          type: "input",
+          name: "engineerGithub",
+          message: "What is the Github username for the engineer?",
+        },
+      ])
+      .then((res) => {
+        const engineer = new Engineer(
+          res.engineerName,
+          res.engineerId,
+          res.engineerEmail,
+          res.engineerGithub
+        );
+        teamMembers.push(engineer);
+        idArray.push(res.engineerId);
+        createTeam();
+      });
+  }
+
+  function addIntern() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "internName",
+          message: "What is the name of the intern?",
+        },
+        {
+          type: "input",
+          name: "internId",
+          message: "What is the id of the intern?",
+        },
+        {
+          type: "input",
+          name: "internEmail",
+          message: "What is the email address for the intern?",
+        },
+        {
+          type: "input",
+          name: "internSchool",
+          message: "What school does the intern attend?",
+        },
+      ])
+      .then((res) => {
+        const intern = new Intern(
+          res.internName,
+          res.internId,
+          res.internEmail,
+          res.internSchool
+        );
+        teamMembers.push(intern);
+        idArray.push(res.internId);
+        createTeam();
+      });
+  }
+
+  function buildTeam() {
+    if (!fs.existsSync(distFolder)) {
+      fs.mkdirSync(distFolder);
+    }
+    fs.writeFileSync(distPath, render(teamMembers), "utf-8");
+  }
+
+  createManager();
 };
 
 startApp();
